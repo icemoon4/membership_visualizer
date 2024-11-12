e020ea294cc017b4aacb9f650ce69c6e502c6404
alexwelsby <alexwelsby@myvuw.ac.nz>
2024-11-12 23:25:16 +1300

    Implemented query parameters, need to change how things handle
    Signed-off-by: alexwelsby <alexwelsby@myvuw.ac.nz>

--- react-visualizer/src/components/Filter.jsx
+++ react-visualizer/src/components/Filter.jsx
@@ -3,59 +3,12 @@ import Member from "./Member";
 
 export default function Filter({ MemberList = [] }) {
   const [query, setQuery] = useState("");
-  const allFields = [
-    [
-      //these expect strings
-      "first_name",
-      "middle_name",
-      "last_name",
-      "email",
-      "best_phone",
-      "mobile_phone",
-      "home_phone",
-      "join_date",
-      "xdate",
-      "membership_type",
-      "monthly_dues_status",
-      "yearly_dues_status",
-      "membership_status",
-      "memb_status_letter",
-      "union_member",
-      "union_name",
-      "union_local",
-      "accomodations",
-      "student_yes_no",
-      "student_school_name",
-      "address1",
-      "address2",
-      "city",
-      "state",
-      "zip",
-      "country",
-      "dsa_chapter",
-      "ydsa_chapter",
-      "list_date",
-      "discord_name",
-      "discord_status",
-    ],
-    [
-      //these expect bools
-      "do_not_call",
-      "p2ptext_optout",
-      "new_member_past_month",
-      "vaccinated",
-      "do_not_text",
-      "do_not_email",
-      "in_chapter",
-    ],
-    ["race", "committee", "region"], //these expect arrays
-  ];
-
+  const [reRender, setReRender] = useState(false);
   let queryParameters = useRef(new Map());
 
   const filteredMembers = MemberList.filter((member) => {
     const fields = member.fields;
-    if (!queryParameters.current.size) {
+    if (!queryParameters.current.size && query !== "") {
       console.log("queryParameters length 0");
       for (const key in fields) {
         const value = fields[key];
@@ -68,14 +21,15 @@ export default function Filter({ MemberList = [] }) {
       }
       return false;
     } //end of 'naive' search w/o terms
+    else if (!queryParameters.current.size && query === "") {
+      return true;
+    }
     //if our user used parameters
     else if (queryParameters.current.size > 0) {
       let matches = 0;
       for (const [key, term] of queryParameters.current) {
         console.log(`HEYYY OVER HERE ${key}:${term} and fields is ${fields}`);
-        if (
-          !fields[key].toString().toLowerCase().includes(term.toLowerCase())
-        ) {
+        if (!fields[key].toString().toLowerCase().includes(term)) {
           //we know this isn't our guy, don't bother completing the loop
           break;
         } else {
@@ -96,7 +50,9 @@ export default function Filter({ MemberList = [] }) {
     queryParameters.current.clear(); //to prevent parameters lingering from previous search
     if (query.includes(":")) {
       //we know the user is using parameters in this case
-      const keyValuePairs = query.match(/(?:[^\s:"']+|['"][^'"]*["'])+/g);
+      const keyValuePairs = query
+        .toLowerCase()
+        .match(/(?:[^\s:"']+|['"][^'"]*["'])+/g);
       console.log(keyValuePairs);
       for (let i = 0; i < keyValuePairs.length - 1; i += 2) {
         queryParameters.current.set(keyValuePairs[i], keyValuePairs[i + 1]);
@@ -105,26 +61,28 @@ export default function Filter({ MemberList = [] }) {
         );
       }
     }
+    setReRender(!reRender);
   }
 
   return (
     <div>
       <form onSubmit={(e) => handleSubmit(e)}>
-        {console.log(MemberList)}
         <input
           value={query}
-          onChange={(e) => setQuery(e.target.value.toLowerCase())}
+          onChange={(e) => setQuery(e.target.value)}
           type="text"
         />
         <button>Go!</button>
       </form>
-      {filteredMembers.length > 0 ? (
-        filteredMembers.map((member) => (
-          <Member key={member.pk} fields={member.fields} />
-        ))
-      ) : (
-        <li>No results found</li>
-      )}
+      <div key={reRender}>
+        {filteredMembers.length > 0 ? (
+          filteredMembers.map((member) => (
+            <Member key={member.pk} fields={member.fields} />
+          ))
+        ) : (
+          <li>No results found</li>
+        )}
+      </div>
     </div>
   );
 }
