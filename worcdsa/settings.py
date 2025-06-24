a1a876bc0f4656a11d71c6f4664a1d761dee79fd
the website devil <98803802+alexwelsby@users.noreply.github.com>
2025-06-24 16:14:39 +1200

    whitenoise added to middleware per Heroku documentation
    Signed-off-by: the website devil <98803802+alexwelsby@users.noreply.github.com>

--- worcdsa/settings.py
+++ worcdsa/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     "simple_history.middleware.HistoryRequestMiddleware",
 ]
