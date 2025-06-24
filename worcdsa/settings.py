f4bfebf07d23463e88e3666730b38f663993dfd3
the website devil <98803802+alexwelsby@users.noreply.github.com>
2025-06-24 16:40:21 +1200

    Added Heroku to allowed hosts
    Signed-off-by: the website devil <98803802+alexwelsby@users.noreply.github.com>

--- worcdsa/settings.py
+++ worcdsa/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2&cb*b%ovh&m7sszmx)k&)ap-j6(hjnn=q0m2+csf@loq#o4%w"
+SECRET_KEY = os.getenv("SECRET_KEY", "localhost")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
 
 #From https://github.com/heroku/python-getting-started/blob/main/gettingstarted/settings.py
 # The `DYNO` env var is set on Heroku CI, but it's not a real Heroku app, so we have to
