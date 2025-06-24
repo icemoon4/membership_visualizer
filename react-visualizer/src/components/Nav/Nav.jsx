6a0144156eb8fb19a5f5f6124f51a73a585c565f
the website devil <98803802+alexwelsby@users.noreply.github.com>
2025-06-24 21:47:25 +1200

    changed admin link
    Signed-off-by: the website devil <98803802+alexwelsby@users.noreply.github.com>

--- react-visualizer/src/components/Nav/Nav.jsx
+++ react-visualizer/src/components/Nav/Nav.jsx
@@ -7,7 +7,7 @@ export default function Nav() {
     <nav className={styles.navBar}>
       <LogoHeader />
       <div className={styles.pageOptions}>
-        <Link to="/admin">Admin</Link>
+        <a href="/admin/">Admin</a>
         <Link to="/app/search">Search</Link>
         <Link to="/app/statistics">Statistics</Link>
         <Link to="/app/logout">Log out</Link>
