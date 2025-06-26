3ea88fcfe411ed9efda833221f74326c8019445e
the website devil <98803802+alexwelsby@users.noreply.github.com>
2025-06-26 23:54:33 +1200

    adding local database info
    Signed-off-by: the website devil <98803802+alexwelsby@users.noreply.github.com>

--- worcdsa/settings.py
+++ worcdsa/settings.py
@@ -149,12 +149,13 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "OPTIONS": {
-                "service": "membership_service",
-                "passfile": ".pgpass",
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST", "localhost"),
+            "PORT": os.getenv("DB_PORT", "5432"),
             }
         }
-    }
 
 
 
