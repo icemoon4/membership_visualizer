243ac43894529ea7bcab068c3e4b1bc6d3daa261
the website devil <98803802+alexwelsby@users.noreply.github.com>
2025-06-26 23:52:05 +1200

    secretkey
    Signed-off-by: the website devil <98803802+alexwelsby@users.noreply.github.com>

--- worcdsa/settings.py
+++ worcdsa/settings.py
@@ -28,7 +28,7 @@ os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", "localhost")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
