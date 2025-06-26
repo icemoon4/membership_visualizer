42b64d267e587d380197b890959aac250be4db0e
the website devil <98803802+alexwelsby@users.noreply.github.com>
2025-06-26 20:41:01 +1200

    allowed_hosts fix
    Signed-off-by: the website devil <98803802+alexwelsby@users.noreply.github.com>

--- worcdsa/settings.py
+++ worcdsa/settings.py
@@ -42,10 +42,7 @@ SESSION_COOKIE_AGE = 300  # seconds (5 minutes)
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_SAVE_EVERY_REQUEST = True
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",  # Allow direct IP access on localhost
-    "localhost",  # Allow using localhost hostname
-]
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 AXES_LOCKOUT_PARAMETERS = ["ip_address", ["username", "user_agent"]]
 AXES_COOLOFF_TIME = 2 #2 hours
@@ -63,8 +60,6 @@ if IS_HEROKU_APP == "DYNO":
 
     USE_X_FORWARDED_HOST = True #because we're using Heroku, which forwards IPs
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-    ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definitn
