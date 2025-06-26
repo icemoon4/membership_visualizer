4d3f90337d956835acbd120fe1121a9df30f5e5f
the website devil <98803802+alexwelsby@users.noreply.github.com>
2025-06-26 23:48:46 +1200

    added dotenv
    Signed-off-by: the website devil <98803802+alexwelsby@users.noreply.github.com>

--- worcdsa/settings.py
+++ worcdsa/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 import os
 import dj_database_url
 from datetime import timedelta
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
