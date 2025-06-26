f8134f338262191be0263e30ac73b1e42ae7faed
the website devil <98803802+alexwelsby@users.noreply.github.com>
2025-06-26 16:13:58 +1200

    as_view()
    Signed-off-by: the website devil <98803802+alexwelsby@users.noreply.github.com>

--- membership/urls.py
+++ membership/urls.py
@@ -4,7 +4,7 @@ from membership.views import LoginView,ValidateTokenView, ValidateRefreshTokenVi
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('api/login/', LoginView, name='login'),
+    path('api/login/', LoginView.as_view(), name='login'),
     path('api/validate-token/', ValidateTokenView.as_view(), name='validate-token'),
     path('api/validate-refresh-token/', ValidateRefreshTokenView.as_view(), name='validate-refresh-token'),
     path("api/<int:member_id>/data", views.detail_json, name="details_json"),
